ccad4ef0652b78e99384fd24d51896c567653364
higginni <higginni@tcd.ie>
2016-01-07 18:52:17 +0000

    added gem and changed views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   include Clearance::User
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
