a8b883e8e296b50a086ec2dfc188f11397a32871
higginni <higginni@tcd.ie>
2016-01-29 12:45:56 +0000

    changing paths to work with a bit more flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "scores/index" => "scores#index"
+  get "pages/profile" => "pages#profile"
   resources :scores
 # get "welcome/test" => "welcome#test"   
 get "pages/score" => "pages#score"
