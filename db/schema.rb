a6b1ca5f4266655b46fa65a0e3c347daca7ab32e
higginni <higginni@tcd.ie>
2016-03-04 00:17:37 +0000

    added name migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201200115) do
+ActiveRecord::Schema.define(version: 20160304001008) do
 
   create_table "enter_scores", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160201200115) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
