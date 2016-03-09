4a92799989b0a0a63e24ed9c8da9a3c7b2dc7b2c
higginni <higginni@tcd.ie>
2016-03-09 20:09:54 +0000

    change of config

--- config/initializers/contact_us.rb
+++ config/initializers/contact_us.rb
@@ -25,6 +25,6 @@ ContactUs.setup do |config|
   config.form_gem = nil
 
   # Configure the redirect URL after a successful submission
-  config.success_redirect = '/'
+  config.success_redirect = '/contacts/new'
 
 end
