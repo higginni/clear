29ab3c195d4855e2259c57b51a10095b92926cb8
higginni <higginni@tcd.ie>
2016-04-10 13:17:46 +0100

    commenting for submission

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,10 +1,10 @@
 class Score < ActiveRecord::Base
-	
+	# allows for image to be attached
 	has_attached_file :image, styles: { large: "500x500>",medium: "300x300>", thumb: "100x100>" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
+    # gives the user attribute the scores
   	belongs_to :user
-
+    # ensures scores can only be 55+ 
   	validates_numericality_of :strokes, :only_integer => true, :greater_than_or_equal_to => 55
 
   	# searchkick
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   # has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   # validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  # means every user owns more than one score. 
   has_many :scores
   
 end
